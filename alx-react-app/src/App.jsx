bd72de3ded3b604230e610c5215ffa61f3c158a4
Omar <nagatsumii@gmail.com>
2024-08-15 19:09:06 +0100

    Create Specific Components in a React Application

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -8,7 +8,7 @@ import UserProfile from './components/UserProfile';
 
 function App() {
   return (
-   <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+    <><Header /><MainContent /><Footer /><UserProfile name="Alice" age="25" bio="Loves hiking and photography" /></>
 )
 }
 export default App
