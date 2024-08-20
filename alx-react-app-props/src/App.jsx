52ebd707fe9c1515368b8584327d68a781d5886b
Omar <nagatsumii@gmail.com>
2024-08-20 20:52:51 +0100

    Refactor Prop Drilling to Use Context API

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,14 +1,14 @@
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
-
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
+  const userData = { name: "Omar Bni", email: "Omar.bni@ALX.com" };
+
   return (
-    <><Header /><MainContent /><Footer /><UserProfile name="Alice" age="25" bio="Loves hiking and photography" /></>
-)
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
 }
-export default App
+
+export default App;
